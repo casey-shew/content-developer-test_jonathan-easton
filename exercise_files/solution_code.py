f79c19a8726d4df7b97e6bf9ef34a8c6388be2dd
jeastonIT <52586961+jeastonIT@users.noreply.github.com>
2019-08-05 22:38:21 -0400

    Created solution_code.py file.

---
+++ exercise_files/solution_code.py
@@ -0,0 +1,12 @@
+color = input('Guess my favorite color: ')
+
+#if statement needs to check the variable color for some string value. Any color is fine.
+if color == 'red':
+    #Some output letting the user know they got the color correct.
+    print('That is it!')
+    print('Go Big Red!')
+else: #else statement
+    #Some output letting the user know they got the color wrong.
+    print('This is not right.')
+
+print ('Goodbye.')
